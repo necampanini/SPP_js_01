107557562268b2d6cfd301f16ac5451988b317d0
Timothy Folds <n00830864@ospreys.unf.edu>
2015-09-17 13:15:07 -0700

    finished, was helpful for remember the syntax

--- SPP_javascript_01/Scripts/thejavascript.js
+++ SPP_javascript_01/Scripts/thejavascript.js
@@ -1,1 +1,33 @@
-﻿
+﻿function doStuff(count) {
+    var factorial = 1;
+
+    for (x = 1; x <= count; x++)
+    {
+        factorial = factorial * x;
+    }
+
+    var output = "The factorial of " + count + " is " + factorial + ".";
+
+    document.getElementById("output").innerHTML = output;
+}
+
+function testInt() {
+    var x = document.getElementById("userInput").value
+
+    if(isNaN(x))
+    {
+        
+        document.getElementById("output").innerHTML = "Insert only a valid number";
+        return;
+    }
+    else
+    {
+        var input = parseInt(x);
+
+        doStuff(input);
+    }
+}
+
+5
+
+5*4 * 3
