1f40d78ccea189b4f7c7eec3a0e2627d38e809ca
Timothy Folds <n00830864@ospreys.unf.edu>
2015-09-17 13:25:23 -0700

    updated some things, accounted for negative numbers error and added comments

--- SPP_javascript_01/Scripts/thejavascript.js
+++ SPP_javascript_01/Scripts/thejavascript.js
@@ -1,33 +1,38 @@
 ﻿function doStuff(count) {
     var factorial = 1;
 
+    //calculates factorial of given number
     for (x = 1; x <= count; x++)
     {
         factorial = factorial * x;
     }
 
+    
     var output = "The factorial of " + count + " is " + factorial + ".";
 
+    //prints factorial message to output div
     document.getElementById("output").innerHTML = output;
 }
 
 function testInt() {
+    //gets user input
     var x = document.getElementById("userInput").value
 
-    if(isNaN(x))
+    //if user input not a number
+    if(isNaN(x) || x < 1)
     {
-        
-        document.getElementById("output").innerHTML = "Insert only a valid number";
+        //inform the dumbass they didn't insert a number greater than 0...
+        document.getElementById("output").innerHTML = "NaN greater than 0";
+        //exit function
         return;
     }
+    //if user is smrt
     else
     {
+        //parses text value to int value
         var input = parseInt(x);
 
+        //calls this function and passes parameter 'input'
         doStuff(input);
     }
 }
-
-5
-
-5*4 * 3
